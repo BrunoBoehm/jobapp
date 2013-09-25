858f2d9facd5e70e928129b07ccd45cbc330afd2
Bruno Boehm <bruno.boehm@gmail.com>
2013-09-25 16:01:29 +0200

    start app and install gem devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jobapp::Application.config.secret_key_base = '56c634d22479ab2991934820d9e72b4966168379b1566ac8f2bb566fb58532811dd309d958c19e88efeddbfc07ed3dfa1eb6e7737199e3adfe66e6746ebd0e76'
